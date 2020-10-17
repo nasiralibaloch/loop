84576f4433599cf23570020795b64315d542d6ce
nasiralibaloch <70817789+nasiralibaloch@users.noreply.github.com>
2020-10-17 22:23:14 +0500

    Add files via upload

---
+++ whle do loop.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int i;
+   int n;
+   int sum=0;
+   cout<<"enter the number "<<endl;
+   cin>>n;
+   for(i=1;i<=n;i++)
+   {
+   	i++;
+	cout<<i<<endl;
+	}
+	
+		cout<<"sum is ="<<sum<<endl;
+
+	return 0;
+}
